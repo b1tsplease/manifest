788b52137824d214f9059f97eee8d3d9572e3dc7
LeticiaYanaguya <leticiahisaeyanaguya@gmail.com>
2020-08-26 18:34:06 -0300

    Add images

--- web/components/Supporters/index.js
+++ web/components/Supporters/index.js
@@ -12,7 +12,39 @@ const ReadManifest = () => (
     </div>
 
     <div className={styles.supportersRight}>
-    
+      <div className={styles.supportersLine}>
+        <a href="https://sobre.quero.com/" target="_blank">
+          <img className={styles.logo} src="/images/supporters/quero_educacao.png" alt="Quero Educação" />
+        </a>
+
+        <a href="https://invoz.org" target="_blank">
+          <img className={styles.logo} src="/images/supporters/invoz.png" alt="INVOZ" />
+        </a>
+
+        <a href="https://www.melhorescola.com.br/" target="_blank">
+          <img className={styles.logo} src="/images/supporters/melhor_escola.png" alt="Melhor Escola" />
+        </a>
+      </div>
+
+      <div className={styles.supportersLine}>
+        <a href="https://www.isemear.org.br/" target="_blank">
+          <img className={styles.logo} src="/images/supporters/instituto_semear.png" alt="Instituto Semear" />
+        </a>
+
+        <a href="https://querobolsa.com.br/" target="_blank">
+          <img className={styles.logo} src="/images/supporters/quero_bolsa.png" alt="Quero Bolsa" />
+        </a>
+
+        <a href="https://regeneracaoglobal.com/home#googtrans(pt)" target="_blank">
+          <img className={styles.logo} src="/images/supporters/regeneracao_global.png" alt="Regeneração Global" />
+        </a>
+      </div>
+
+      <div className={styles.supportersLine}>
+        <a href="https://www.iefuturo.org.br/" target="_blank">
+          <img className={styles.logo} src="/images/supporters/instituto_empreendedor_futuro.png" alt="Instituto Empreendedor do Futuro" />
+        </a>
+      </div>
     </div>
   </div>
 );
