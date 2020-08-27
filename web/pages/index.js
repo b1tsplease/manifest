500dbfea1219389e419434b5bd06fbb93ec79a33
Rafael Julio <hello.rjulio@gmail.com>
2020-08-26 22:22:19 -0300

    refactor(web): add web.dev measurement optimizations

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,22 +1,34 @@
-import Head from 'next/head';
-import { useState } from 'react';
-import Content from '../components/Content';
-import DepositionsSection from '../components/DepositionsSection';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import IFrame from '../components/IFrame';
-import InfoBullets from '../components/InfoBullets';
-import Modal from '../components/Modal';
-import ReadManifest from '../components/ReadManifest';
-import Supporters from '../components/Supporters';
-import Midia from '../components/Midia';
-import { YouTube } from '../components/Video';
+import Head from "next/head";
+import { useState } from "react";
+import Content from "../components/Content";
+import DepositionsSection from "../components/DepositionsSection";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import IFrame from "../components/IFrame";
+import InfoBullets from "../components/InfoBullets";
+import Midia from "../components/Midia";
+import Modal from "../components/Modal";
+import ReadManifest from "../components/ReadManifest";
+import Supporters from "../components/Supporters";
+import { YouTube } from "../components/Video";
 
 export default function Home() {
-
   const [depositions, setDepositions] = useState([
-    { type: 'comment', comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ', author: 'Anderson Santos', ocuppation: 'Developer' },
-    { type: 'video', videoUrl: 'https://www.youtube.com/embed/rHax1Pg-iSM', imageUrl: '/images/video-deposition.png', imageAlt: 'Vídeo do Fulano falando em algum lugar', author: 'Anderson Santos', ocuppation: 'Developer' },
+    {
+      type: "comment",
+      comment:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+      author: "Anderson Santos",
+      ocuppation: "Developer",
+    },
+    {
+      type: "video",
+      videoUrl: "https://www.youtube.com/embed/rHax1Pg-iSM",
+      imageUrl: "/images/video-deposition.png",
+      imageAlt: "Vídeo do Fulano falando em algum lugar",
+      author: "Anderson Santos",
+      ocuppation: "Developer",
+    },
   ]);
 
   const [isVideoModalVisible, setIsVideoModalVisible] = useState(false);
@@ -29,11 +41,18 @@ export default function Home() {
   const handleManifestModalDismiss = () => setIsManifestModalVisible(false);
 
   return (
-    <div>
+    <>
       <Head>
         <title>Internet pela Educação</title>
+        <meta
+          name="description"
+          description="A educação remota em tempos de pandemia evidenciou o óbvio: O grande número de excluídos digitais"
+        />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Header />
@@ -53,7 +72,6 @@ export default function Home() {
           <IFrame src="/files/manifesto_pela_educacao.pdf"></IFrame>
         </Modal>
 
-
         <Hero onClickWatchVideo={handleVideoModalOpen} />
 
         <InfoBullets />
@@ -66,6 +84,6 @@ export default function Home() {
 
         <Midia />
       </Content>
-    </div>
-  )
-};
+    </>
+  );
+}
